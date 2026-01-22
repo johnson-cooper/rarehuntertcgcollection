d3a1fb4a51239a73930b57fa2fe5fd4edf37ca90
Cooper Johnson <129237833+johnson-cooper@users.noreply.github.com>
2026-01-21 22:37:15 -0500

    Add files via upload

--- ygostore/wsgi.py
+++ ygostore/wsgi.py
@@ -1,4 +1,9 @@
 import os
+from whitenoise import WhiteNoise
+from django.conf import settings
 from django.core.wsgi import get_wsgi_application
+
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ygostore.settings')
 application = get_wsgi_application()
+application = WhiteNoise(application, root=str(settings.MEDIA_ROOT), prefix='media/')
