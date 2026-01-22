e5a78567f915d49166a66b0e7426ab62c7f3685b
Cooper Johnson <129237833+johnson-cooper@users.noreply.github.com>
2026-01-22 17:39:36 -0500

    Add files via upload

--- collection/urls.py
+++ collection/urls.py
@@ -5,11 +5,6 @@ urlpatterns = [
     path('products/', views.api_products, name='api-products'),
     path('create-checkout-session/', views.create_checkout_session, name='create-checkout-session'),
     path('card/<int:card_id>/', views.card_detail, name='card-detail'),
-     # Static / info pages
-    path('about/', views.about, name='about'),
-    path('terms/', views.terms, name='terms'),
-    path('privacy/', views.privacy, name='privacy'),
-    path('success/', views.success, name='success'),
-    path('cancel/', views.cancel, name='cancel'),
+
     
 ]
