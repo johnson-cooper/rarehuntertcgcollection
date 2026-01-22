f29f189007872cec3c5f2148eeed3b1724eee295
Cooper Johnson <129237833+johnson-cooper@users.noreply.github.com>
2026-01-21 21:36:37 -0500

    Add files via upload

--- ygostore/settings.py
+++ ygostore/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # add near the top, after SecurityMiddleware
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
