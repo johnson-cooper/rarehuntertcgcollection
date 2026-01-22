c5306687cafd57cf4250087463a35fa08ddc96bc
Cooper Johnson <129237833+johnson-cooper@users.noreply.github.com>
2026-01-22 15:55:18 -0500

    Add files via upload

--- collection/urls.py
+++ collection/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('products/', views.api_products, name='api-products'),
     path('create-checkout-session/', views.create_checkout_session, name='create-checkout-session'),
+    path('card/<int:card_id>/', views.card_detail, name='card-detail'),
     
 ]
