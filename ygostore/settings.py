ba9439734324221904ed95f23456c64e9262103a
Cooper Johnson <129237833+johnson-cooper@users.noreply.github.com>
2026-01-21 23:57:04 -0500

    Add files via upload

--- ygostore/settings.py
+++ ygostore/settings.py
@@ -15,7 +15,7 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['collection.rarehuntertcg.com
 SECRET_KEY = env('SECRET_KEY', default='changeme')
 
 CSRF_TRUSTED_ORIGINS = [
-    'collection.rarehuntertcg.com'
+    'https://collection.rarehuntertcg.com'
 ]
 
 
@@ -83,4 +83,4 @@ MEDIA_URL = '/media/'
 # Stripe configuration
 STRIPE_SECRET_KEY = env('STRIPE_SECRET_KEY', default='')
 STRIPE_WEBHOOK_SECRET = env('STRIPE_WEBHOOK_SECRET', default='')
-BASE_URL = env('BASE_URL', default='collection.rarehuntertcg.com')
+BASE_URL = env('BASE_URL', default='https://collection.rarehuntertcg.com')
